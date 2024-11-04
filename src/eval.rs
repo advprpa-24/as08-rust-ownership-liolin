59d56cc59b6aa9275e48f3973aed36c3c02bfbb8
Olivier Lischer <olivier.lischer@liolin.ch>
2024-11-04 12:23:05 +0100

    Solve task 1 and partially task 2

--- src/eval.rs
+++ src/eval.rs
@@ -4,23 +4,33 @@ use std::collections::HashSet;
 /// Evaluates a term:
 /// In order to simplify the term as much as possible, we use a call-by-value strategy.
 /// We even evaluate the body of an abstraction eagerly.
-/// 
+///
 /// The evaluation rules are as follows:
 /// - A variable evaluates to itself.
 /// - An abstraction evaluates to itself, even the body is eagerly evaluated.
-/// - An application evaluates the left side, then the right side, 
+/// - An application evaluates the left side, then the right side,
 ///   and applies the left side to the right side by substitution.
-///   Examples: 
+///   Examples:
 ///   `x` evaluates to `x`.
 ///   `λx. x` evaluates to `λx. x`.
 ///   `(λx. x) y` evaluates to `y`.
 ///   `(λx. (λy. x)) z` evaluates to `λy. z`.
 ///   `(λx. (λy. x)) a b` evaluates to `a`.
 pub fn eval(term: &Term) -> Term {
-    term.clone()
-    // TODO: "Implement the eval function")
+    match term {
+        Term::Var(identifier) => Term::Var(identifier.into()),
+        Term::Abs(identifier, term) => Term::Abs(identifier.into(), Box::new(eval(term))),
+        Term::App(term_l, term_r) => {
+            let term_l = eval(term_l);
+            let term_r = eval(term_r);
+            match term_l {
+                Term::Var(_) => Term::App(Box::new(term_l), Box::new(term_r)),
+                Term::Abs(identifier, term) => substitute(&term, &identifier, &term_r),
+                Term::App(term_l, term_r) => todo!(),
+            }
+        }
+    }
 }
-
 
 /// Replace all occurrences of a variable `var` in a `term` with `replacement`.
 pub fn substitute(term: &Term, var: &str, replacement: &Term) -> Term {
@@ -135,6 +145,15 @@ mod tests {
     }
 
     #[test]
+    fn test_eval_var() {
+        // x -> x
+        let term = var("x");
+        let evaluated = eval(&term);
+        let expected = var("x");
+        assert_eq!(evaluated, expected);
+    }
+
+    #[test]
     fn test_eval_simple_application() {
         // (λx. x) y -> y
         let term = app(abs("x", var("x")), var("y"));
@@ -174,6 +193,14 @@ mod tests {
         let term = app(abs("x", app(abs("y", var("y")), var("x"))), var("z"));
         let evaluated = eval(&term);
         let expected = var("z");
+        assert_eq!(evaluated, expected);
+    }
+
+    #[test]
+    fn test_eval_more_complex_application() {
+        let term = app(app(abs("x", abs("y", var("x"))), var("a")), var("b"));
+        let evaluated = eval(&term);
+        let expected = var("a");
         assert_eq!(evaluated, expected);
     }
 }
--- src/pretty.rs
+++ src/pretty.rs
@@ -3,13 +3,61 @@ use std::fmt;
 
 /// Pretty prints a term.
 pub fn pretty_print(term: &Term) -> String {
-    format!("{:?}", term)
-    // TODO: Implement pretty printing for lambda calculus terms.
+    match term {
+        Term::Var(identifier) => identifier.into(),
+        Term::Abs(identifier, term) => format!("(λ{identifier}. {term})"),
+        Term::App(term_l, term_r) => format!("{term_l} {term_r}"),
+    }
 }
 
 /// Display trait implementation for Term.
 impl fmt::Display for Term {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         write!(f, "{}", pretty_print(self))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_pp_var() {
+        let term = var("x");
+        let expected = "x";
+        let pp = pretty_print(&term);
+        assert_eq!(pp, expected);
+    }
+
+    #[test]
+    fn test_pp_abs() {
+        let term = abs("x", var("x"));
+        let expected = "(λx. x)";
+        let pp = pretty_print(&term);
+        assert_eq!(pp, expected);
+    }
+
+    #[test]
+    fn test_pp_app() {
+        let term = app(abs("x", var("x")), var("y"));
+        let expected = "(λx. x) y";
+        let pp = pretty_print(&term);
+        assert_eq!(pp, expected);
+    }
+
+    #[test]
+    fn test_pp_complex() {
+        let term = app(abs("x", app(var("x"), var("y"))), var("z"));
+        let expected = "(λx. x y) z";
+        let pp = pretty_print(&term);
+        assert_eq!(pp, expected);
+    }
+
+    #[test]
+    fn test_pp_more_complex() {
+        let term = app(app(abs("x", abs("y", var("x"))), var("a")), var("b"));
+        let expected = "(λx. (λy. x)) a b";
+        let pp = pretty_print(&term);
+        assert_eq!(pp, expected);
     }
 }
